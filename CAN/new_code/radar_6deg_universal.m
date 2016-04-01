4dd02045df96f844f99347c8290875d6ef9030ef
Yudong Tian <yudong.tian@nasa.gov>
2016-04-01 13:26:48 -0400

    bug fix for pcm decoding

--- CAN/new_code/radar_6deg_universal.m
+++ CAN/new_code/radar_6deg_universal.m
@@ -15,14 +15,6 @@ flight_day = str2double((flight_directory(10:11)));
 flight_month = str2double((flight_directory(8:9)));
 flight_year = str2double((flight_directory(4:7)));
 
-%% Initialize key options
-flagHalf = 1;
-% flag to process data for fore or aft half-scan
-fore = 1;
-
-% flag to plot h or v pol data on Google Earth
-GoogleEarth  = 0;    %YDT now do in plot_radar.m
-
 % sometimes timestamp went to 1/1/1970
 
 timeRadInit = datenum('10/1/2015 00:00:00');
@@ -30,6 +22,7 @@ timeRadFinal = datenum('11/30/2015 00:00:00');
 
 % RDRRETURN_20151030T181002.mat
 files = dir('RDRRETURN_*.mat');
+%files = dir('RDRRETURN_20151030T185000.mat');
 files_to_process = 1:length(files);
 %%
 sample_period = 500e-6; %sec
@@ -77,7 +70,7 @@ f = 1262500000; % Hz
 c = 299792458; % m/s
 Pt = 10^(47/10);
 %YDT Gt = 17.7^(47/10);
-Gt = 17.7; 
+Gt = 10^(17.7/10); 
 Gr = Gt;
 
 radar_factor =  Pt * Gt * ( c^2 / (4*pi*f)^2 ) * 1/(4*pi) * Gr;
@@ -130,17 +123,6 @@ trk = AngleTrackdeg(timeGeoInd);
 
 ned_vals = 238;   %m, from Albert Wu's code
 AGL = alt - ned_vals;
-
-%% roll flag and limits
-flagRoll = 1;
-% Aircraft roll angle limits if removing data during a turn
-rollmin = -5;
-rollmax = 5;
-
-
-[time_total, az_total, lat_total, lon_total, hh_total, vv_total, hv_total, ...
- vh_total, alt_total, trk_total, roll_total, hdg_total] = deal([]);
-
 
 %%
 for k = files_to_process
@@ -219,7 +201,8 @@ for k = files_to_process
     % if it has a 1 in its 4s bit, it's V transmitting.
     % the nomenclature is as follows: indstart_xy where x is the transmit pol
     % and y is the receive pol
-    if pcm(1) == 0
+    %YDT bug? if pcm(1) == 0
+    if pcm_sep(1) == 0
         indstart_hh = 1;
         indstart_vh = 2;
         indstart_hv = 1;
@@ -231,13 +214,16 @@ for k = files_to_process
         indstart_vv = 1;
     end
     
-    increment = 134;% ~= 6 deg in scan angle
+    % increment = 134;% ~= 6 deg in scan angle
+    increment = 132;% ~= 6 deg in scan angle    YDT: make it multiple of 4 
     
     numlines = floor(length(az_all)/increment);
     
     % initialize variables
     [azavg, latavg, lonavg, mean_hh, mean_vv, mean_hv, mean_vh, altavg, trkavg, rollavg, hdgavg, timeavg] = deal(zeros(numlines-1,1));
     
+    'before j loop  ...'
+    %keyboard
     for j = 1:numlines-1
         % get radar data points for each range bin
         ind = (increment*(j-1)+1):increment*j;
@@ -297,7 +283,7 @@ for k = files_to_process
                     surfaceDistance = surface_radius(indmid) + surface_ranges;
                     % range from SLAP (at altitude) to each range gate is determined by the
                     % Pythagorean thereom.
-                    R = sqrt ( surfaceDistance.^ 2 + altavg(j)^2);
+                    R = sqrt ( surfaceDistance.^2 + altavg(j).^2);
                     
                     % interpolate counts values to determine power in mW
                     powerReceived_h_pol = interp1(cal_hpol_counts, power_mW_h, double(usefulCounts) );
@@ -310,7 +296,7 @@ for k = files_to_process
                     % % convert power from dBm to mW
                     % powerReceived_h_pol = 10.^(powerReceived_h_pol/10);
 
-                    powerReceived_h_pol = powerReceived_h_pol* 10.^(data_step_hh(i)/10);
+                    powerReceived_h_pol = powerReceived_h_pol* 10.^(data_step_hh(i)./10);
                     
                     % equation in linear (mW) space to solve for sigma
                     sumPandR = sum(powerReceived_h_pol.*R.^4);
@@ -344,7 +330,7 @@ for k = files_to_process
                     surfaceDistance = surface_radius(indmid) + surface_ranges;
                     % range from SLAP (at altitude) to each range gate is determined by the
                     % Pythagorean thereom.
-                    R = sqrt ( surfaceDistance.^ 2 + altavg(j)^2);
+                    R = sqrt ( surfaceDistance.^2 + altavg(j).^2);
                     
                     % interpolate counts values to receive pol to determine power in mW
                     powerReceived_v_pol = interp1(cal_vpol_counts, power_mW_v, double(usefulCounts) );
@@ -357,7 +343,7 @@ for k = files_to_process
                     % convert power from dBm to mW
                     %  powerReceived_v_pol = 10.^(powerReceived_v_pol/10);
 
-                    powerReceived_v_pol = powerReceived_v_pol* 10.^(data_step_hv(i)/10);
+                    powerReceived_v_pol = powerReceived_v_pol* 10.^(data_step_hv(i)./10);
 
 
                     % equation in linear (mW) space to solve for sigma
@@ -393,7 +379,7 @@ for k = files_to_process
                     surfaceDistance = surface_radius(indmid) + surface_ranges;
                     % range from SLAP to each range gate is determined by the
                     % Pythagorean thereom.
-                    R = sqrt ( surfaceDistance.^ 2 + altavg(j)^2);
+                    R = sqrt ( surfaceDistance.^2 + altavg(j).^2);
                     
                     % interpolate counts values to determine power in mW\
                     powerReceived_v_pol = interp1(cal_vpol_counts, power_mW_v, double(usefulCounts) );
@@ -406,7 +392,7 @@ for k = files_to_process
                     % % convert power from dBm to mW
                     % powerReceived_v_pol = 10.^(powerReceived_v_pol/10);
 
-                    powerReceived_v_pol = powerReceived_v_pol* 10.^(data_step_vv(i)/10);
+                    powerReceived_v_pol = powerReceived_v_pol* 10.^(data_step_vv(i)./10);
 
                     % equation in linear (mW) space to solve for sigma
                     sumPandR = sum(powerReceived_v_pol.*R.^4);
@@ -439,7 +425,7 @@ for k = files_to_process
                     surfaceDistance = surface_radius(indmid) + surface_ranges;
                     % range from SLAP to each range gate is determined by the
                     % Pythagorean thereom.
-                    R = sqrt ( surfaceDistance.^ 2 + altavg(j)^2);
+                    R = sqrt ( surfaceDistance.^2 + altavg(j).^2);
                     
                     % interpolate counts values to receive pol to determine power in mW
                     
@@ -453,7 +439,7 @@ for k = files_to_process
                     % % convert power from dBm to mW
                     % powerReceived_h_pol = 10.^(powerReceived_h_pol/10);
 
-                    powerReceived_h_pol = powerReceived_h_pol* 10.^(data_step_vh(i)/10);
+                    powerReceived_h_pol = powerReceived_h_pol* 10.^(data_step_vh(i)./10);
                     
                     % equation in linear (mW) space to solve for sigma
                     sumPandR = sum(powerReceived_h_pol.*R.^4);
@@ -474,124 +460,16 @@ for k = files_to_process
         mean_vh(j) = mean(sigma0vh);
         mean_vv(j) = mean(sigma0vv);
     end
+    %%%%%%%%%%% j loop done
+
+    'j loop done ...'
+     %keyboard
     
     % save data for future processing
     save(['6deg_', files(k).name(1:end-4), '.mat'], 'mean_hh', 'mean_vv', 'latavg', ...
               'lonavg', 'altavg', 'mean_hv', 'mean_vh','hdgavg', 'rollavg', ...
               'trkavg', 'timeavg', 'azavg')
-    
-    time_total = [time_total; timeavg];
-    hh_total = [hh_total; mean_hh];
-    vv_total = [vv_total; mean_vv];
-    hv_total = [hv_total; mean_hv];
-    vh_total = [vh_total; mean_vh];
-    az_total = [az_total; azavg];
-    lat_total = [lat_total; latavg];
-    lon_total = [lon_total; lonavg];
-    alt_total = [alt_total; altavg];
-    trk_total = [trk_total; trkavg];
-    roll_total = [roll_total; rollavg];
-    hdg_total = [hdg_total; hdgavg];
 
 end   % file loop 
-    %-------------------------------------------------------------------
-    if GoogleEarth
 
-
-       % only plot on either the fore or aft half scan if flagHalf is on      
-        if flagHalf
-            % initialize flag_half_scan as zeros
-            flag_half_scan = zeros(length(trk_total),1);
-            
-            for k = 1:length(trk_total)
-                % get 180 degrees of scan width around track angle to get front half
-                scan_lower = trk_total(k) - 90;
-                scan_upper = trk_total(k) + 90;
-                scan_upper(scan_upper>360) = scan_upper(scan_upper>360) - 360;
-                scan_lower(scan_lower<0) = scan_lower(scan_lower<0) + 360;
-                
-                % get indices of azimuth angles within the 180 degrees of the front
-                % half
-                flagf1 = az_total(k)<scan_upper;
-                flagf2 = az_total(k)>=scan_lower;
-                
-                if trk_total(k) < 180;
-                    if scan_lower > 180 && (flagf1 || flagf2)
-                        flag_half_scan(k) = k;
-                    elseif scan_lower <= 180 && (flagf1 && flagf2)
-                        flag_half_scan(k) = k;
-                    end
-                else
-                    if scan_upper > 180 && (flagf1 && flagf2)
-                        flag_half_scan(k) = k;
-                    elseif scan_upper <= 180 && (flagf1 || flagf2)
-                        flag_half_scan(k) = k;
-                    end
-                end
-            end
-            
-            % turn flag_half_scan into a vector of logicals where ones are indices of
-            % fore half scan data points
-            flag_half_scan = flag_half_scan>0;
-            
-            if ~fore
-                % get indices of aft half scan as the opposite of the indices of the fore
-                % half scan if the flag for the fore half scan is turned off
-                flag_half_scan = ~flag_half_scan;
-            end
-            
-        else
-            % if flagHalf is off, to plot fore and aft half scans together, get all logical ones
-            flag_half_scan = find(lat_total>0);
-        end
-        
-        % get geolocation data for fore or aft half scans using flag_half_scan
-        trk_half_scan = trk_total(flag_half_scan);
-        lon_half_scan = lon_total(flag_half_scan);
-        lat_half_scan = lat_total(flag_half_scan);
-        roll_half_scan = roll_total(flag_half_scan);
-        vv_half_scan = vv_total(flag_half_scan);
-        hh_half_scan = hh_total(flag_half_scan);
-        hv_half_scan = hv_total(flag_half_scan);
-        vh_half_scan = vh_total(flag_half_scan);
-        alt_half_scan = alt_total(flag_half_scan);
-        time_half_scan = time_total(flag_half_scan);
-        az_half_scan = az_total(flag_half_scan);
-        hdg_half_scan = hdg_total(flag_half_scan);
-        
-        Tb_hh = hh_half_scan;
-        Tb_hv = hv_half_scan;
-        Tb_vv = vv_half_scan;
-        Tb_vh = vh_half_scan;
-        
-        Lat = lat_half_scan;
-        Long = lon_half_scan;
-        Alt = alt_half_scan;
-        Roll = roll_half_scan; 
-        
-        % if flagRoll is set on, perform the roll filter
-        if flagRoll
-            indRoll = find(roll_half_scan < rollmax & roll_half_scan > rollmin);
-            Tb_hv = Tb_hv(indRoll);
-            Tb_vh = Tb_vh(indRoll);
-            Tb_vv = Tb_vv(indRoll);
-            Tb_hh = Tb_hh(indRoll);
-            Lat = lat_half_scan(indRoll);
-            Long = lon_half_scan(indRoll);
-            Alt = alt_half_scan(indRoll);
-            Roll = roll_half_scan(indRoll); 
-        end
-
-        minBin= - 100;
-        maxBin= -70;
-        
-        %save_kml('radar_Fore_HV.kml', Tb_hv, Lat, Long, Alt, Roll, beam_width_h_pol, minBin, maxBin) 
-        %save_kml('radar_Fore_VH.kml', Tb_vh, Lat, Long, Alt, Roll, beam_width_v_pol, minBin, maxBin) 
-        %save_kml('radar_Fore_HV.kml', Tb_hv, Lat, Long, Alt, Roll, beam_width_h_pol, minBin, maxBin) 
-        save_kml('radar_Fore_HH.kml', Tb_hh, Lat, Long, Alt, Roll, beam_width_h_pol, minBin, maxBin) 
-
-        
-    end  % if Google Earth 
-    %-------------------------------------------------------------------
 quit
-
