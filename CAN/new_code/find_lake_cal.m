390cf8cfb9f08c22cdb0ae9c81b06c05580b3f9c
Yudong Tian <yudong.tian@nasa.gov>
2016-05-02 16:09:36 -0400

    Working version: find lake flyover period, plot it, find Tb from buoy data, and print mean values.

---
+++ CAN/new_code/find_lake_cal.m
@@ -0,0 +1,125 @@
+%% Find and print lake calibration data
+%  Find lake cal period from roll angle readings, 
+%  then find Tbs from buoy data table
+%    
+
+clear
+
+% min time-span of cal data to use -- to get rid of airplane quick turn-arounds  
+cal_dt = 30;   % secs 
+
+%sample interval: 1/15 sec
+dt = 1/15.0;  
+
+dw = cal_dt / dt;    %anything narrower than this is not lake flyover 
+
+
+%lower limit: sometimes the time range went berserk 
+time0 = datenum('2015-Oct-1 00:00:00', 'yyyy-mmm-dd HH:MM:SS'); 
+
+% control plot range 
+countLow = 2.5e6; 
+countHigh = 4e6; 
+
+% read in lake Tb data 
+tdata = readtable('/home/ytian/smos/SLAP-DATA-yudong/CAN/Lake_buoys/buoy1_all_data.csv', ...
+                  'ReadVariableNames', false); 
+
+buoy_time = datenum(tdata{:, 1}, 'mm/dd/yyyy HH:MM');   
+buoy_temp = tdata{:, 3}; 
+
+%to plot
+plot(buoy_time, buoy_temp)
+datetick('x', 6)
+print('lake_tb-vs-time', '-dpng');
+
+filesRad = dir('RAD*FB_v4.mat');
+files_to_process = 1:length(filesRad);
+
+timeAll = [];
+h2antAll = [];
+v2antAll = [];
+rollAll = [];
+
+for i = files_to_process
+   % time data from FB.mat and h2ant and v2ant from FB_m2data.mat
+   load(filesRad(i).name)                       % RADTELEM_20151108T212000_FB_v4.mat
+   fdate=filesRad(i).name(10:17);
+
+   timeAll = [timeAll; timeavg];
+   h2antAll = [h2antAll; havg];
+   v2antAll = [v2antAll; vavg];
+   rollAll = [rollAll; rollavg];
+
+end
+
+
+tgood  = find(timeAll > time0);
+timeAll = timeAll(tgood);
+h2antAll = h2antAll(tgood);
+v2antAll = v2antAll(tgood);
+rollAll = rollAll(tgood);
+
+% find lake flyover from roll angle data, and find the closest match in lake Tb data
+
+r40=find( (rollAll > -43 & rollAll < -37) | (rollAll > 37 & rollAll < 43) );
+
+% get a 0/1 flag vector 
+roll01 = rollAll*0; 
+roll01(r40) = 1; 
+
+[pks, locs, ws, ps] = findpeaks(roll01); 
+
+for i=1:length(ws) 
+  if (ws(i) < dw ) 
+   %wipe out this narrow step 
+   wipe=locs(i):(locs(i)+ws(i)-1);  
+   roll01(wipe) = 0; 
+  end 
+end 
+
+cal=find(roll01);   % indices of usable cal data 
+lake_time=timeAll(cal); 
+lake_h2=h2antAll(cal); 
+lake_v2=v2antAll(cal); 
+lake_roll=rollAll(cal); 
+lake_tb=interp1(buoy_time, buoy_temp, lake_time);
+
+ x1= min(lake_time); 
+ x2= max(lake_time); 
+
+  figure
+  subplot(3, 1, 1)
+  plot(lake_time, lake_v2, 'b')
+  axis([x1 x2 countLow countHigh])
+  title(['Time series of V- and H-pol counts ' fdate])
+  datetick('x', 15)
+  hold on
+  plot(lake_time, lake_h2, 'g')
+
+  subplot(3, 1, 2)
+  plot(lake_time, lake_h2-lake_v2)
+  axis([x1 x2 0e5 7e5])
+  title('Time series of H - V counts')
+  hold on
+  plot([x1 x2], [2e5 2e5], 'g')   % 2e5 is about the difference producing TbH-TbV=0.
+  datetick('x', 15)
+
+  subplot(3, 1, 3)
+  plot(lake_time, lake_roll)
+  axis([x1 x2 -50 50])
+  title('Time series of roll angels (deg)')
+  datetick('x', 15)
+
+  print(['lake_cal_counts_ts-' fdate], '-dpng');
+
+  %print the cal data 
+
+  cal_date = datestr(mean(lake_time), 'yyyy-mmm-dd HH:MM:SS');
+
+  %disp('----------------- after outlier removal ---------------------------------------------------------------')
+  fprintf('%s, %10.3f, %10.0f, %10.0f', ...
+          cal_date, [mean(lake_tb), mean(lake_v2), mean(lake_h2)]); 
+  fprintf('\n');  
+
+  quit 
